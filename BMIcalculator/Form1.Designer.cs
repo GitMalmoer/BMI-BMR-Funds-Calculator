310bf0ed40174e4970ac7efb71db4af55e981672
Marcin2 <marcinjunka@gmail.com>
2022-09-24 20:34:18 +0200

    Few buttons

--- BMIcalculator/Form1.Designer.cs
+++ BMIcalculator/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(25, 26);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(82, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Your Name";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(27, 72);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(54, 20);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Height";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(165, 23);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 27);
+            this.txtName.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lblName;
+        private Label lblHeight;
+        private TextBox txtName;
+        private TextBox textBox1;
     }
 }
